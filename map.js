f5fe9248e5f622910b630f844cd004c427a91d7d
ellamartini <144989040+ellamartini@users.noreply.github.com>
2026-07-01 23:34:14 -0700

    extreme makeover code edition
    put all assets in a folder and cleaned up all code

--- map.js
+++ map.js
@@ -1,15 +1,16 @@
 const aboutDiv = document.getElementById('aboutDiv');
-const btnText = document.getElementById('btnText');
-const aboutBtn = document.getElementById('aboutBtn');
+const btnText = document.getElementById('closeBtnText');
+const expandBtn = document.getElementById('expandBtn');
 
 btnText.addEventListener("click", () => {
     aboutDiv.style.visibility = 'hidden';
     btnText.style.visibility = 'hidden';
-    aboutBtn.style.visibility = 'visible';
+    expandBtn.style.visibility = 'visible';
 })
 
-aboutBtn.addEventListener("click", () => {
+expandBtn.addEventListener("click", () => {
     aboutDiv.style.visibility = 'visible';
     btnText.style.visibility = 'visible';
-    aboutBtn.style.visibility = 'hidden';
+    expandBtn.style.visibility = 'hidden';
+    expandBtn.style.animation = 'stop';
 })
